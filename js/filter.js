ab7e6f99ebbe57f7500531c0cae4741b28ba9808
Woody Tai <109840996+woodytai@users.noreply.github.com>
2025-03-14 21:45:08 +0800

    Launch!

---
+++ js/filter.js
@@ -0,0 +1,32 @@
+function getElements(id) {
+  if (typeof id == "object") {
+    return [id];
+  } else {
+    return document.querySelectorAll(id);
+  }
+};
+
+function filterHTML(id, sel, filter) {
+  var a, b, c, i, ii, iii, hit;
+  a = getElements(id);
+  for (i = 0; i < a.length; i++) {
+    b = a[i].querySelectorAll(sel);
+    for (ii = 0; ii < b.length; ii++) {
+      hit = 0;
+      if (b[ii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+        hit = 1;
+      }
+      c = b[ii].getElementsByTagName("*");
+      for (iii = 0; iii < c.length; iii++) {
+        if (c[iii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+          hit = 1;
+        }
+      }
+      if (hit == 1) {
+        b[ii].style.display = "";
+      } else {
+        b[ii].style.display = "none";
+      }
+    }
+  }
+};
